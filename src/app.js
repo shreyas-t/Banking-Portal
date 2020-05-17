235394dbc0371b04d328a156fb3d0e611ffd2fe4
T <shreyas.t@sap.com>
2020-05-17 16:42:03 +0530

    Added new file data.js to handle data

--- src/app.js
+++ src/app.js
@@ -4,11 +4,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
+const { accounts, users, writeJSON } = require('./data.js');
 
 app.engine('ejs', require('ejs').__express)
 app.set('views',path.join(__dirname, 'views'));
@@ -52,8 +48,7 @@ app.get('/transfer', (req, res)=>{
 app.post('/transfer', (req, res)=>{
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -64,12 +59,9 @@ app.get('/payment', (req, res) => res.render('payment', {account: accounts.credi
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
-
-
 
 
 app.listen(3000,()=>{
