715c32dc0ff012fd02acb97f315e5f0a5ebe2da6
T <shreyas.t@sap.com>
2020-05-17 16:11:02 +0530

    Added account transfer and payemnt

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
     res.render('index', { 
         'title': 'Account Summary',
@@ -41,11 +44,31 @@ app.get('/checking',(req, res)=>{
     });
 });
 
+app.get('/transfer', (req, res)=>{
+    res.render('transfer');
+});
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
+app.post('/post', (req, res)=>{
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer Completed'});
+});
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
+
+
 
 
 app.listen(3000,()=>{
